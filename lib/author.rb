504429ff9c387a9b0a87dfcd581c79cc68c2ca51
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-17 01:02:58 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,24 @@
 class Author
+  attr_accessor :name, :artist, :title, :songs
 
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(song)
+    song = Song.new(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def self.song_count
+    @songs.count
+  end
 
 end
